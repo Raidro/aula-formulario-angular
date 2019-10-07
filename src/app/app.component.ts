4f5bd249f5029d4df2dc9d930414d6ab7c6f59ed
Raidro <mateus_sat@hotmail.com>
2019-10-07 18:29:42 -0300

    consertdo o erro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,42 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
+class Cliente {
+
+  nome: string;
+  email: string;
+  profissao: string;
+
+  constructor() {
+    this.nome = '';
+    this.email = '';
+    this.profissao = '';
+  }
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
-  cliente: new Cliente();
+  cliente = new Cliente();
 
-  profissoes = ['Engenheiro', 'Dentista', 'Professor']; // nome da propriedade
+profissoes = ['Engenheiro', 'Dentista', 'Professor']; // nome da propriedade
 
 
   salvar(form: NgForm) {
 
-  this.cliente.nome = form.value.inputNome;
-  this.cliente.email = form.value.inputNome;
-  this.cliente.profissao = form.value.inputNome;
+    this.cliente.nome = form.value.inputNome;
+    this.cliente.email = form.value.inputNome;
+    this.cliente.profissao = form.value.inputNome;
 
   }
 
 }
 
-class Cliente {
-  nome: string;
-  email: string;
-  profissao: string;
 
-}
